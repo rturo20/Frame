3963492949f66400436d6d41740d4c9109d8073c
pelmasri <paesanoelmasri@gmail.com>
2018-03-11 11:30:32 +0000

    3.0.006

--- Sources/Frame.swift
+++ Sources/Frame.swift
@@ -63,9 +63,9 @@ public struct Frame {
 			//}	
 		
 		switch (key, action) {
-		case (GLFW_KEY_E, GLFW_PRESS) == true:
+		case (GLFW_KEY_E, GLFW_PRESS):
 			print("E")
-		case (GLFW_KEY_Q, GLFW_PRESS) == true:
+		case (GLFW_KEY_Q && GLFW_PRESS):
 			glfwSetWindowShouldClose(window, GL_TRUE)
 		default:
 			print("Default")
